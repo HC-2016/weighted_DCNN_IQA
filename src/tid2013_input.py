4d4fd1945aa035773f3147c8c5e51075083087f8
HC-2016 <chenhuang@pku.edu.cn>
2017-06-27 11:43:43 +0800

    Initial commit

---
+++ src/tid2013_input.py
@@ -0,0 +1,445 @@
+"""
+@File         : tid2013_input.py
+@Time         : 2017/6/22 
+@Author       : Chen Huang
+@Update       : 
+@Discription  : convert original data to standard tensorflow data '.tfrecords';
+                generate image and label batch.
+"""
+
+
+# from __future__ import unicode_literals
+# from __future__ import print_function
+# from __future__ import division
+
+import os
+import gzip
+import numpy
+import PIL.Image as Image
+
+from tensorflow.python.platform import gfile
+import tensorflow as tf
+
+# dirs
+DATA_DIR = '../data'
+LOG_DIR = '../logs'
+
+# path of mnist
+DIR = 'D:/Data'
+DATABASE = 'tid2013'
+MOS_PATH = DIR + '/' + DATABASE + '/mos_with_names.txt'
+REF_PATH = DIR + '/' + DATABASE + '/reference_images/'
+DIS_PATH = DIR + '/' + DATABASE + '/distorted_images/'
+
+# information of mnist
+HEIGHT = 384
+WIDTH = 512
+DEPTH = 3
+
+PATCH_SIZE = 32
+NUM_PATCHES_PER_IMAGE = 32
+
+TRAIN_DATA_NUM = 15
+VAL_DATA_NUM = 5
+TEST_DATA_NUM = 5
+NUM_PER_IMAGE = 5*24
+
+def _read32(bytestream):
+    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+
+def dense_to_one_hot(labels_dense, num_classes):
+    """Convert class labels from scalars to one-hot vectors."""
+    num_labels = labels_dense.shape[0]
+    index_offset = numpy.arange(num_labels) * num_classes
+    labels_one_hot = numpy.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+
+def extract_images(f):
+    """Extract the images into a 4D uint8 numpy array [index, y, x, depth].
+
+    Args:
+        :param f: file object. a file object that can be passed into a gzip reader.
+
+    Returns:
+        :return data: A 4D uint8 numpy array [index, y, x, depth].
+
+    Raises:
+        :exception ValueError: If the bytestream does not start with 2051.
+    """
+    print('Extracting', f.name)
+    with gzip.GzipFile(fileobj=f) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2051:
+            raise ValueError('Invalid magic number %d in MNIST image file: %s' %
+                             (magic, f.name))
+        num_images = _read32(bytestream)
+        rows = _read32(bytestream)
+        cols = _read32(bytestream)
+        buf = bytestream.read(rows * cols * num_images)
+        data = numpy.frombuffer(buf, dtype=numpy.uint8)
+        data = data.reshape(num_images, rows, cols, 1)
+
+        return data
+
+
+def extract_labels(f, one_hot=False, num_classes=10):
+    """Extract the labels into a 1D uint8 numpy array [index].
+
+    Args:
+        :param f: file object. A file object that can be passed into a gzip reader.
+        :param one_hot: bool. Does one hot encoding for the result.
+        :param num_classes: int. Number of classes for the one hot encoding.
+
+    Returns:
+        :returns labels: ndarray. a 1D uint8 numpy array.
+
+    Raises:
+        :exception ValueError: If the bystream doesn't start with 2049.
+    """
+    print('Extracting', f.name)
+    with gzip.GzipFile(fileobj=f) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2049:
+            raise ValueError('Invalid magic number %d in MNIST label file: %s' %
+                             (magic, f.name))
+        num_items = _read32(bytestream)
+        buf = bytestream.read(num_items)
+        labels = numpy.frombuffer(buf, dtype=numpy.uint8)
+        if one_hot:
+            return dense_to_one_hot(labels, num_classes)
+
+        return labels
+
+
+def load_img(path, gray_scale=False):
+    """ Load image and convert to Image object.
+
+    Args:
+        :param path:str. the path of the image file.
+        :param gray_scale:bool. gray or color.
+
+    Return:
+        :return img:Image object. an instance of Image object.
+    """
+    img = Image.open(path)
+    if gray_scale:
+        img = img.convert('L')
+    else:
+        img = img.convert('RGB')
+
+    return img
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def convert_to(x, y, z, filename):
+    """Converts data to tfrecords.
+
+    Args:
+      :param x, y: list - [img1, img2, ...].
+                    img: ndarray.
+      :param name: str. 
+    """
+    if not gfile.Exists(filename):
+        print('Writing', filename)
+        writer = tf.python_io.TFRecordWriter(filename)
+        for index in range(NUM_PER_IMAGE):
+            image_x = x[index].tostring()
+            image_y = y[index].tostring()
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'label': _float_feature(z[index]),
+                'image_x': _bytes_feature(image_x),
+                'image_y': _bytes_feature(image_y)
+            }))
+            writer.write(example.SerializeToString())
+        writer.close()
+
+
+def load_data():
+    """Load database, convert to Examples and write the result to TFRecords."""
+    # Load data
+    text_file = open(MOS_PATH, "r")
+    lines = text_file.readlines()
+    text_file.close()
+
+    ref_img_set = []
+    dis_img_set = []
+    mos_set = []
+    for line in lines:
+        mos, name = line.rstrip().split(' ', 2)  # (5.51, i01_01_1.bmp)
+        ref_name = name.split('_')[0] + '.bmp'  # i01.bmp
+
+        path = REF_PATH + ref_name.lower()
+        ref_img_set.append(numpy.asarray(load_img(path, gray_scale=False), dtype=numpy.uint8))  # ndarray(0,255)
+        path = DIS_PATH + name.lower()
+        dis_img_set.append(numpy.asarray(load_img(path, gray_scale=False), dtype=numpy.uint8))
+        mos_set.append(float(mos))
+
+    # Convert to Examples and write the result to TFRecords.
+    for i in range(TRAIN_DATA_NUM+VAL_DATA_NUM+TEST_DATA_NUM):
+        x = [ref_img_set[j] for j in numpy.arange(i * NUM_PER_IMAGE, (i + 1) * NUM_PER_IMAGE)]
+        y = [dis_img_set[j] for j in numpy.arange(i * NUM_PER_IMAGE, (i + 1) * NUM_PER_IMAGE)]
+        z = [mos_set[j] for j in numpy.arange(i * NUM_PER_IMAGE, (i + 1) * NUM_PER_IMAGE)]
+
+        # import matplotlib.pyplot as plt
+        # plt.figure('subplot')
+        # plt.subplot(2, 2, 1)
+        # plt.imshow(ref_img_set[0] / 255.0)
+        # plt.subplot(2, 2, 2)
+        # plt.imshow(dis_img_set[0] / 255.0)
+        # plt.subplot(2, 2, 3)
+        # plt.imshow(ref_img_set[4] / 255.0)
+        # plt.subplot(2, 2, 4)
+        # plt.imshow(dis_img_set[4] / 255.0)
+        # plt.show()
+
+        if not gfile.Exists(os.path.join(DATA_DIR)):
+            os.makedirs(os.path.join(DATA_DIR))
+
+        filename = os.path.join(DATA_DIR, 'image_' + str(i) + '.tfrecords')
+        convert_to(x, y, z, filename)
+
+
+def read_and_decode(filename_queue):
+    """Reads and parses examples from data files .tfrecords.
+
+    Args:
+        :param filename_queue: queue. A queue of strings with the filenames to read from. 
+
+    Returns:
+        :return result: DataRecord. An object representing a single example, with the following fields:
+            label: an int32 Tensor.
+            image_x, image_y: a [height*width*depth] uint8 Tensor with the image data.
+    """
+
+    class DataRecord(object):
+        pass
+
+    result = DataRecord()
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(
+        serialized_example,
+        # Defaults are not specified since both keys are required.
+        features={
+            'label': tf.FixedLenFeature([], tf.float32),
+            'image_x': tf.FixedLenFeature([], tf.string),
+            'image_y': tf.FixedLenFeature([], tf.string),
+        })
+
+    # Convert from a scalar string tensor to a uint8 tensor
+    result.image_x = tf.decode_raw(features['image_x'], tf.uint8)
+    result.image_y = tf.decode_raw(features['image_y'], tf.uint8)
+    result.image_x.set_shape([HEIGHT*WIDTH*DEPTH])
+    result.image_y.set_shape([HEIGHT*WIDTH*DEPTH])
+
+    # Convert label from a scalar uint8 tensor to an int32 scalar.
+    result.label = features['label']
+
+    return result
+
+
+def _generate_image_and_label_batch(image, label, min_queue_examples,
+                                    batch_size, shuffle):
+    """Construct a queued batch of images and labels.
+
+    Args:
+        :param image: tuple - (image_x, image_y). 
+                imagex, image_y: 3-D Tensor of [height, width, 3] of type.float32.
+        :param label: 1-D Tensor of type.float32
+        :param min_queue_examples: int32, minimum number of samples to retain 
+        in the queue that provides of batches of examples.
+        :param batch_size: Number of images per batch.
+        :param shuffle: boolean indicating whether to use a shuffling queue.
+
+    Returns:
+        :return images_x, images_y: Images. 4D tensor of [batch_size, height, width, 3] size.
+        :return labels: Labels. 1D tensor of [batch_size] size.
+    """
+    # Create a queue that shuffles the examples, and then
+    # read 'batch_size' images + labels from the example queue.
+    num_preprocess_threads = 8
+    if shuffle:
+        images_x, images_y, label_batch = tf.train.shuffle_batch(
+            [image[0], image[1], label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_size,
+            min_after_dequeue=min_queue_examples)
+    else:
+        images_x, images_y, label_batch = tf.train.batch(
+            [image[0], image[1], label],
+            batch_size=batch_size,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * batch_size)
+
+    # Display the training images in the visualizer.
+    tf.summary.image('batch_images_x', tensor=images_x, max_outputs=4)
+    tf.summary.image('batch_images_y', tensor=images_y, max_outputs=4)
+
+    return images_x, images_y, label_batch
+
+
+def random_sample(images_x, images_y, patch_size, num_patches):
+    """random sample patch pairs from image pairs.
+    
+    Args:
+        :param images_x, images_y: tensor - (batch_size, height, width, depth). 
+        :param patch_size: int. 
+        :param num_patches: int. we crop num_patches patches from each image pair.
+        
+    Returns:
+        :return patches_x, patches_y: tensor - (batch_size*num_patches, patch_size, patch_size, depth). 
+    """
+    patches_x = []
+    patches_y = []
+
+    images_xy = tf.concat([images_x, images_y], axis=3)
+    for i in range(images_x.get_shape()[0].value):
+        for j in range(num_patches):
+            # Randomly crop a [height, width] section of the image.
+            patch_xy = tf.random_crop(images_xy[i, :, :, :], [patch_size, patch_size, DEPTH*2])
+
+            patches_x.append(patch_xy[:, :, :3])
+            patches_y.append(patch_xy[:, :, 3:])
+
+    patches_x = tf.convert_to_tensor(value=patches_x, dtype=tf.float32, name='sampled_patches_x')
+    patches_y = tf.convert_to_tensor(value=patches_y, dtype=tf.float32, name='sampled_patches_y')
+
+    return patches_x, patches_y
+
+
+def distorted_inputs(filenames, batch_size):
+    """Construct distorted input for training using the Reader ops.
+
+    Args:
+        :param filenames: list - [str1, str2, ...].
+        :param batch_size: int. 
+
+    Returns:
+       :returns: tuple - (patches_x, patches_y, labels).
+                patches_x, patches_y: tensors - (batch_size*num_patches, patch_size, patch_size, depth). 
+                lables: tensors - (batch_size).
+    """
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file: ' + f)
+
+    with tf.variable_scope('input'):
+        # Create a queue that produces the filenames to read.
+        filename_queue = tf.train.string_input_producer(string_tensor=filenames)
+
+        # Even when reading in multiple threads, share the filename queue.
+        result = read_and_decode(filename_queue)
+
+        # OPTIONAL: Could reshape into a image and apply distortionshere.
+        reshaped_image_x = tf.reshape(result.image_x, [HEIGHT, WIDTH, DEPTH])
+        reshaped_image_y = tf.reshape(result.image_y, [HEIGHT, WIDTH, DEPTH])
+
+        # Convert from [0, 255] -> [-0.5, 0.5] floats.
+        distorted_image_x = tf.cast(reshaped_image_x, tf.float32) * (1. / 255) - 0.5
+        distorted_image_y = tf.cast(reshaped_image_y, tf.float32) * (1. / 255) - 0.5
+
+        # # Randomly flip the image horizontally.
+        # distorted_image = tf.image.random_flip_left_right(distorted_image)
+        #
+        # # Because these operations are not commutative, consider randomizing
+        # # the order their operation.
+        # distorted_image = tf.image.random_brightness(distorted_image,
+        #                                              max_delta=63)
+        # distorted_image = tf.image.random_contrast(distorted_image,
+        #                                            lower=0.2, upper=1.8)
+        #
+        # # Subtract off the mean and divide by the variance of the pixels.
+        # distorted_image = tf.image.per_image_standardization(distorted_image)
+        #
+        # # Set the shapes of tensors.
+        # distorted_image.set_shape([patch_size, patch_size, result.depth])
+        # result.label.set_shape([1])
+        label = result.label
+
+        # Ensure that the random shuffling has good mixing properties.
+        min_queue_examples = 1000
+        print('Filling queue with %d mnist images before starting to train or validation. '
+              'This will take a few minutes.' % min_queue_examples)
+
+        # Generate a batch of images and labels by building up a queue of examples.
+        images_x, images_y, labels = \
+            _generate_image_and_label_batch(image=(distorted_image_x, distorted_image_y), label=label,
+                                               min_queue_examples=min_queue_examples,
+                                               batch_size=batch_size,
+                                               shuffle=True)
+
+        # Random crop patches from images
+        patches_x, patches_y = random_sample(images_x, images_y, PATCH_SIZE, NUM_PATCHES_PER_IMAGE)
+
+        # Display the training images in the visualizer.
+        tf.summary.image('patches_x', tensor=patches_x, max_outputs=4)
+        tf.summary.image('patches_y', tensor=patches_y, max_outputs=4)
+
+        return patches_x, patches_y, labels
+
+
+def inputs(filenames, batch_size):
+    """Construct input without distortion for MNIST using the Reader ops.
+
+    Args:
+        :param filenames: list - [str1, str2, ...].
+        :param batch_size: int. 
+
+    Returns:
+       :returns: tuple - (images, labels).
+                images: tensors - [batch_size, height*width*depth].
+                lables: tensors - [batch_size].
+    """
+    for f in filenames:
+        if not tf.gfile.Exists(f):
+            raise ValueError('Failed to find file: ' + f)
+
+    with tf.variable_scope('input_evaluation'):
+        # Create a queue that produces the filenames to read.
+        filename_queue = tf.train.string_input_producer(string_tensor=filenames)
+
+        # Even when reading in multiple threads, share the filename
+        # queue.
+        result = read_and_decode(filename_queue)
+
+        # OPTIONAL: Could reshape into a image and apply distortionshere.
+        reshaped_image_x = tf.reshape(result.image_x, [HEIGHT, WIDTH, DEPTH])
+        reshaped_image_y = tf.reshape(result.image_y, [HEIGHT, WIDTH, DEPTH])
+
+        # Convert from [0, 255] -> [-0.5, 0.5] floats.
+        image_x = tf.cast(reshaped_image_x, tf.float32) * (1. / 255) - 0.5
+        image_y = tf.cast(reshaped_image_y, tf.float32) * (1. / 255) - 0.5
+        label = result.label
+
+        # Ensure that the random shuffling has good mixing properties.
+        min_queue_examples = 500
+        print('Filling queue with %d mnist images before starting to train or validation. '
+              'This will take a few minutes.' % min_queue_examples)
+
+        # Generate a batch of images and labels by building up a queue of examples.
+        images_x, images_y, labels = \
+            _generate_image_and_label_batch(image=(image_x, image_y), label=label,
+                                               min_queue_examples=min_queue_examples,
+                                               batch_size=batch_size,
+                                               shuffle=True)
+
+        # Random crop patches from images
+        patches_x, patches_y = random_sample(images_x, images_y, PATCH_SIZE, NUM_PATCHES_PER_IMAGE)
+
+        return patches_x, patches_y, labels
---
+++ src/train.py
@@ -0,0 +1,231 @@
+"""
+@File         : wdcnn.py
+@Time         : 2017/6/22 
+@Author       : Chen Huang
+@Update       : 
+@Discription  : A tensorflow implementation of the network in "Bosse S, Maniry D, Müller K R, et al. Deep Neural 
+Networks for No-Reference and Full-Reference Image Quality Assessment[J]. arXiv preprint arXiv:1612.01697, 2016."
+"""
+
+
+# from __future__ import unicode_literals
+# from __future__ import print_function
+# from __future__ import division
+
+import os
+import numpy
+from scipy import stats
+import tensorflow as tf
+
+import tid2013, tid2013_input
+
+
+ORDER = numpy.random.permutation(tid2013_input.TRAIN_DATA_NUM +
+                                     tid2013_input.VAL_DATA_NUM + tid2013_input.TEST_DATA_NUM)
+
+
+def evaluate(type, global_step=None):
+    """Evaluation for a number of steps."
+    
+    Args:
+        :param type: str - 'train'/'val'/'test'
+        :param global_step: int.
+    Returns:
+        :return mae: float.
+    """
+    assert type in ['train', 'val', 'test']
+
+    graph = tf.Graph()
+    with graph.as_default() as g:
+        keep_prob = tf.placeholder(tf.float32, name='ratio')
+        # Input images and labels.
+        if type == 'train':
+            filenames = [os.path.join(tid2013_input.DATA_DIR, 'image_' + str(i) + '.tfrecords')
+                     for i in ORDER[0:tid2013_input.TRAIN_DATA_NUM]]
+            num_data = tid2013_input.TRAIN_DATA_NUM * tid2013_input.NUM_PER_IMAGE
+        elif type == 'val':
+            filenames = [os.path.join(tid2013_input.DATA_DIR, 'image_' + str(i) + '.tfrecords')
+                     for i in ORDER[tid2013_input.TRAIN_DATA_NUM:tid2013_input.TRAIN_DATA_NUM+tid2013_input.VAL_DATA_NUM]]
+            num_data = tid2013_input.VAL_DATA_NUM * tid2013_input.NUM_PER_IMAGE
+        elif type == 'test':
+            filenames = [os.path.join(tid2013_input.DATA_DIR, 'image_' + str(i) + '.tfrecords')
+                     for i in ORDER[tid2013_input.TRAIN_DATA_NUM+tid2013_input.VAL_DATA_NUM:
+                tid2013_input.TRAIN_DATA_NUM + tid2013_input.VAL_DATA_NUM + tid2013_input.TEST_DATA_NUM]]
+            num_data = tid2013_input.TEST_DATA_NUM * tid2013_input.NUM_PER_IMAGE
+
+        patches_x, patches_y, labels = tid2013.inputs(filenames=filenames)
+
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        scores = tid2013.inference(patches_x, patches_y, keep_prob)
+
+        total_loss = tid2013.loss_func(scores, labels)
+
+        # Restore the moving average version of the learned variables for eval.
+        variable_averages = tf.train.ExponentialMovingAverage(
+            tid2013.MOVING_AVERAGE_DECAY)
+        variables_to_restore = variable_averages.variables_to_restore()
+        saver = tf.train.Saver(variables_to_restore)
+
+        # Build the summary operation based on the TF collection of Summaries.
+        summary_op = tf.summary.merge_all()
+        summary_writer = tf.summary.FileWriter(os.path.join(tid2013_input.LOG_DIR, type), g)
+
+    with tf.Session(graph=graph) as sess:
+        # ckpt = tf.train.get_checkpoint_state(tid2013_input.LOG_DIR)
+        if type == 'val':
+            checkpoint_file = os.path.join(tid2013_input.LOG_DIR, 'temp_model.ckpt')
+        else:
+            checkpoint_file = os.path.join(tid2013_input.LOG_DIR, 'best_model.ckpt')
+
+        saver.restore(sess, checkpoint_file)
+
+        # if ckpt and ckpt.model_checkpoint_path:
+        #     # Restores from checkpoint
+        #     saver.restore(sess, ckpt.model_checkpoint_path)
+        #     # Assuming model_checkpoint_path looks something like:
+        #     #   /my-favorite-path/cifar10_train/model.ckpt-0,
+        #     # extract global_step from it.
+        #     global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+        # else:
+        #     print('No checkpoint file found')
+        #     return
+
+        # Start the queue runners.
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        score_set = []
+        label_set = []
+        loss_set = []
+        step = 0
+        num_iter = int(numpy.ceil(num_data / tid2013.BATCH_SIZE))
+
+        while step < num_iter and not coord.should_stop():
+            loss_hat, scores_hat, labels_hat = sess.run([total_loss, scores, labels], feed_dict={keep_prob: 1.0})
+            score_set.append(scores_hat)
+            label_set.append(labels_hat)
+            loss_set.append(loss_hat)
+            step += 1
+
+        score_set = numpy.reshape(numpy.asarray(score_set), (-1,))
+        label_set = numpy.reshape(numpy.asarray(label_set), (-1,))
+        loss_set = numpy.reshape(numpy.asarray(loss_set), (-1,))
+
+        # Compute evaluation metric.
+        mae = loss_set.mean()
+        srocc = stats.spearmanr(score_set, label_set)[0]
+        krocc = stats.stats.kendalltau(score_set, label_set)[0]
+        plcc = stats.pearsonr(score_set, label_set)[0]
+        rmse = numpy.sqrt(((score_set - label_set) ** 2).mean())
+        mse = ((score_set - label_set) ** 2).mean()
+        print("%s: MAE: %.3f\t SROCC: %.3f\t KROCC: %.3f\t PLCC: %.3f\t RMSE: %.3f\t MSE: %.3f"
+              % (type, mae, srocc, krocc, plcc, rmse, mse))
+
+        summary = tf.Summary()
+        summary.ParseFromString(sess.run(summary_op, feed_dict={keep_prob: 1.0}))
+        summary.value.add(tag='MAE_' + type, simple_value=mae)
+        summary.value.add(tag='SROCC_' + type, simple_value=srocc)
+        summary.value.add(tag='RMSE_' + type, simple_value=rmse)
+        summary_writer.add_summary(summary, global_step)
+
+        coord.request_stop()
+        coord.join(threads)
+
+    return mae
+
+
+def train():
+    """Train the network for a number of steps.
+    
+    Args:
+        :param order: ndarray - (1,).
+    """
+    graph = tf.Graph()
+    with graph.as_default():
+        # Create a variable to count the number of train() calls. For multi-GPU programs, this equals the
+        # number of batches processed * num_gpus.
+        global_step = tf.get_variable(
+            'global_step', [],
+            initializer=tf.constant_initializer(0), trainable=False)
+        keep_prob = tf.placeholder(tf.float32, name='ratio')
+
+        # Input images and labels.
+        filenames = [os.path.join(tid2013_input.DATA_DIR, 'image_' + str(i) + '.tfrecords')
+                     for i in ORDER[0:tid2013_input.TRAIN_DATA_NUM]]
+        patches_x, patches_y, labels = tid2013.distorted_inputs(filenames)
+
+        # Build a Graph that computes predictions from the inference model.
+        scores = tid2013.inference(patches_x, patches_y, keep_prob)
+
+        # Add to the Graph the Ops for loss calculation.
+        total_loss = tid2013.loss_func(scores, labels)
+
+        # Add to the Graph the Ops that calculate and apply gradients.
+        train_op = tid2013.train_func(data_num=tid2013_input.TRAIN_DATA_NUM*tid2013_input.NUM_PER_IMAGE,
+                                      total_loss=total_loss, global_step=global_step)
+
+        # Create a saver for writing training checkpoints.
+        saver = tf.train.Saver()
+
+        # Build the summary Tensor based on the TF collection of Summaries.
+        summary_op = tf.summary.merge_all()
+
+        # The op for initializing the variables.
+        init_op = tf.group(tf.global_variables_initializer(),
+                           tf.local_variables_initializer())
+
+    with tf.Session(graph=graph) as sess:
+        # Initialize the variables (the trained variables and the epoch counter).
+        sess.run(init_op)
+
+        # Start input enqueue threads.
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+        # Instantiate a SummaryWriter to output summaries and the Graph.
+        summary_writer = tf.summary.FileWriter(tid2013_input.LOG_DIR, sess.graph)
+
+        # Start the training loop.
+        min_loss = 1000
+        best_epoch = 0
+        iters_per_epoch = numpy.ceil(tid2013_input.TRAIN_DATA_NUM * tid2013_input.NUM_PER_IMAGE / tid2013.BATCH_SIZE)
+        for step in range(tid2013.MAX_STEPS):
+            _, loss_value = sess.run([train_op, total_loss], feed_dict={keep_prob: 0.5})
+            assert not numpy.isnan(loss_value), 'Model diverged with loss = NaN'
+
+            # Write the summaries and print an overview fairly often.
+            if step % iters_per_epoch == 0:
+                # Print status to stdout.
+                print('Epoch %d (Step %d): loss = %.3f' % (step / iters_per_epoch, step, loss_value))
+                # Update the events file.
+                summary_str = sess.run(summary_op, feed_dict={keep_prob: 0.5})
+                summary_writer.add_summary(summary_str, step)
+                summary_writer.flush()
+
+            # Save a checkpoint and evaluate the model periodically.
+            if step % iters_per_epoch == 0 or (step + 1) == tid2013.MAX_STEPS:
+                checkpoint_file = os.path.join(tid2013_input.LOG_DIR, 'temp_model.ckpt')
+                saver.save(sess, checkpoint_file)
+
+                # evaluate(type='train')
+                val_loss = evaluate('val', step)
+
+                if val_loss < min_loss:
+                    min_loss = val_loss
+                    best_epoch = step / iters_per_epoch
+                    print('best epoch %d with min loss %.3f' % (best_epoch, min_loss))
+
+                    checkpoint_file = os.path.join(tid2013_input.LOG_DIR, 'best_model.ckpt')
+                    saver.save(sess, checkpoint_file)
+
+        coord.request_stop()
+        coord.join(threads)
+
+        evaluate(type='test')
+
+
+if __name__ == '__main__':
+    tid2013_input.load_data()
+
+    train()
